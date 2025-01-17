5cec25bcf2fe790573cbc2c1e1828d5297e71f25
eixcosxsinxi <wesleyboud@gmail.com>
2025-01-16 22:02:45 -0500

    This is my finished Grade.java

---
+++ Grader.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Grader {
+	public static void main(String[] args) {
+		var scan = new Scanner(System.in);
+
+		int n;
+		n = scan.nextInt(); // this is the first number in the grades.txt which will tell me how many grades there are
+		int original[] = new int[n];
+		int rounded[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			original[i] = scan.nextInt();
+		}
+		for (int j = 0; j < n; j++) {
+			if (original[j] >= 40) {
+				if (original[j] % 5 <= 3) {
+					rounded[j] = original[j];
+				} else {
+					rounded[j] = original[j];
+					rounded[j] += (5 - (original[j] % 5));
+				}
+			} else {
+				rounded[j] = original[j];
+			}
+		}
+		for (int k = 0; k < n; k++) {
+			System.out.println(rounded[k]);
+		}
+		scan.close();
+	}
+}
